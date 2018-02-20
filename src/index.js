17f7bbb3816f258a3557ee3005f105986cc8293c
odragel <olga.dragel@gmail.com>
2018-02-21 00:34:45 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    var biggestDivToFive = Math.floor(number / 5);
+    var result = biggestDivToFive;
+    for (var i = 0; i < biggestDivToFive; i++) {
+        var temp = biggestDivToFive - i;
+        if (temp % 5 == 0){
+            var amountOfFives = 0;
+            while(temp % 5 == 0){
+                amountOfFives++;
+                temp = temp / 5;
+            }
+            result=result + amountOfFives;
+            i = i+4;
+        }
+    }
+    return result;
+
 }
